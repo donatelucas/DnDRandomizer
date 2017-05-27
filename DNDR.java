91f53581856c0f0fb2142a314b127e1d2f1ede62
Luke <knightfication@gmail.com>
2017-05-26 20:30:32 -0500

    Functional Main Method with both options

--- DNDR.java
+++ DNDR.java
@@ -8,18 +8,50 @@ public class DNDR
         Government gov = new Government();
         Scanner scan = new Scanner(System.in);
         Dungeon dungeon = new Dungeon();
-        int amount; String answer; boolean satisfied = false;
-        roll.d20();
-        roll.storedRoll();
-        roll.resultPrint();
-        dungeon.DeathTrap(roll.storedRoll());
+        int amount; String answer; boolean satisfied = false; int choice;
 
-        /*
-        roll.d100();
-        roll.storedRoll();
-        roll.resultPrint();
-        gov.formsOfGovernment(roll.storedRoll());
+        System.out.println("Welcome to the D&D Randomizer by d4wnbreaker.");
+        System.out.println("At this moment, we just have two options, more options are being added daily.");
+        System.out.println("You can suggest additions directly to him: @d4wnbreaker.");
+        System.out.println("What do you want to roll?");
+        System.out.println("Dungeons (1), Forms of Government (2)");
+        choice = scan.nextInt();
 
-        */
+        switch(choice)
+        {
+            case 1:
+                roll.d20();
+                roll.storedRoll();
+                roll.resultPrint();
+                dungeon.DeathTrap(roll.storedRoll());
+
+                roll.d20();
+                roll.storedRoll();
+                roll.resultPrint();
+                dungeon.Purpose(roll.storedRoll());
+
+                roll.d20();
+                roll.storedRoll();
+                roll.resultPrint();
+                dungeon.Maze(roll.storedRoll());
+
+                roll.d20();
+                roll.storedRoll();
+                roll.resultPrint();
+                dungeon.Mine(roll.storedRoll());
+
+                roll.d100();
+                roll.storedRoll();
+                roll.resultPrint();
+                dungeon.PlanarGate(roll.storedRoll());
+            break;
+
+            case 2:
+                roll.d100();
+                roll.storedRoll();
+                roll.resultPrint();
+                gov.formsOfGovernment(roll.storedRoll());
+            break;
+        }
     }
 }
